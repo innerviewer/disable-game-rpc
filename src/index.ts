cca7075a6903e4630d2c454a99efa554aa123930
innerviewer <innerviewerr@gmail.com>
2025-08-01 22:44:12 +0200

    (Plugin) First working version.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import { Injector, webpack, Logger } from "replugged";
-import { flux } from "replugged/common";
 
 const injector = new Injector();
 const logger = Logger.plugin("HideGameActivity");
 
-
 export async function start(): Promise<void> {
   logger.log("Starting HideGameActivity plugin...");
-
-  flux.Store.getAll().forEach((store) => {
-    logger.log("Store found: ", store.getName())
-  });
 
   const store = webpack.getByStoreName("RunningGameStore") as any;
   if (store === undefined) {
@@ -18,37 +12,33 @@ export async function start(): Promise<void> {
     return;
   }
 
-  logger.log("RunningGameStore found: ", store);
-  logger.log("Currently running games: ", store.getRunningGames())
+  //logger.log("RunningGameStore found: ", store);
+  //logger.log("Currently running games: ", store.getRunningGames())
 
   injector.instead(store, "isDetectionEnabled", () => {
-    logger.log("[HideGameActivity] Suppressed isDetectionEnabled.");
+    logger.log("Suppressed isDetectionEnabled.");
     return false;
   });
 
   injector.instead(store, "getRunningGames", () => {
-    logger.log("[HideGameActivity] Suppressed getRunningGames.");
+    logger.log("Suppressed getRunningGames.");
     return [];
   });
 
   injector.instead(store, "getVisibleRunningGames", () => {
-    logger.log("[HideGameActivity] Suppressed getVisibleRunningGames.");
+    logger.log("Suppressed getVisibleRunningGames.");
     return [];
   });
 
   injector.instead(store, "getVisibleGame", () => {
-    logger.log("[HideGameActivity] Suppressed getVisibleGame.");
+    logger.log("Suppressed getVisibleGame.");
     return undefined;
   });
 
-
-  const gamestore = webpack.getByStoreName("GameStore") as any;
-  logger.log("GameStore found: ", gamestore);
-
-  logger.log()
-  //injector.instead(gamestore, "")
+  logger.log("HideGameActivity plugin started successfully.");
 }
 
 export function stop(): void {
+  injector.uninjectAll();
 }
 
