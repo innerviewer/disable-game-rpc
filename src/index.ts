5453aadca4108139e3986ac8405efce129f653f3
innerviewer <innerviewerr@gmail.com>
2025-08-01 22:07:41 +0200

    (Replugged) Testing different approaches.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,54 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, webpack, Logger } from "replugged";
+import { flux } from "replugged/common";
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const injector = new Injector();
+const logger = Logger.plugin("HideGameActivity");
+
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) =>
-      | {
-          name: string;
-        }
-      | undefined;
-  }>(webpack.filters.byProps("getChannel"));
+  logger.log("Starting HideGameActivity plugin...");
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
+  flux.Store.getAll().forEach((store) => {
+    logger.log("Store found: ", store.getName())
+  });
+
+  const store = webpack.getByStoreName("RunningGameStore") as any;
+  if (store === undefined) {
+    logger.error("RunningGameStore not found.");
+    return;
   }
+
+  logger.log("RunningGameStore found: ", store);
+  logger.log("Currently running games: ", store.getRunningGames())
+
+  injector.instead(store, "isDetectionEnabled", () => {
+    logger.log("[HideGameActivity] Suppressed isDetectionEnabled.");
+    return false;
+  });
+
+  injector.instead(store, "getRunningGames", () => {
+    logger.log("[HideGameActivity] Suppressed getRunningGames.");
+    return [];
+  });
+
+  injector.instead(store, "getVisibleRunningGames", () => {
+    logger.log("[HideGameActivity] Suppressed getVisibleRunningGames.");
+    return [];
+  });
+
+  injector.instead(store, "getVisibleGame", () => {
+    logger.log("[HideGameActivity] Suppressed getVisibleGame.");
+    return undefined;
+  });
+
+
+  const gamestore = webpack.getByStoreName("GameStore") as any;
+  logger.log("GameStore found: ", gamestore);
+
+  logger.log()
+  //injector.instead(gamestore, "")
 }
 
 export function stop(): void {
-  inject.uninjectAll();
 }
+
